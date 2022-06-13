44bf2b25ca9245c3df701c967884ebd67519387e
WooHaem <whm1001@4-m.co.kr>
2022-06-13 17:30:04 +0900

    20220613

--- src/App.js
+++ src/App.js
@@ -1,64 +1,48 @@
-import React, {useRef, useState} from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
+/* eslint-disable */
+
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
-  });
-  const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ]);
 
-  const nextId = useRef(4);
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    // setUsers([...users, user]);  // 첫번째 방법 - spread 연산자 이용
-    setUsers(users.concat(user));  // 두번째 방법 - concat함수 사용 (기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열을 만들어 줌)
-
-    setInputs({
-      username: '',
-      email: ''
-    });
-    nextId.current += 1;
+  let [글제목, 글제목변경] = useState(['남자코트 추천' , '강남 우동 맛집', '리액트 독학']);
+  let [따봉, 따봉변경] = useState(0);
+  let posts = '강남 고기 맛집';
+  
+  function 제목바꾸기() {
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경( newArray );
   }
+
   return (
-    <>
-      <CreateUser 
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} />
-    </>
+    <div className='App'>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+
+      <button onClick={ 제목바꾸기 }>버튼</button>
+      
+      <div className='list'>
+        <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className='list'>
+        <h3> { 글제목[1] }</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+
+      <div className='list'>
+        <h3> { 글제목[2] }</h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
+
+    </div>
   );
     
 }
